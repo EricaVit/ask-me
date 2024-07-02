02fad46e7a152d5ed3fc04920bae70f6f6866135
EricaVit <ericavitoria891@gmail.com>
2024-07-02 11:14:47 -0300

    Implementação da página inicial

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
 const express = require('express')
 const app = express()
+const bodyparser = require('body-parser')
+const connection = require('./database/database')
+const Pergunta = require('./database/Pergunta')
 const port = 3000
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Banco de dados conectado com sucesso!")
+    }).catch((msgErro) => {
+        console.log(msgErro)
+    })
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/', (reg, res) =>{
-    res.render('index')
+app.use(bodyparser.urlencoded({ extended: false }))
+app.use(bodyparser.json())
+
+app.get('/', (req, res) => {
+    //SELECT * FROM pergunta
+    Pergunta.findAll({raw: true, order:[
+        ['id', 'DESC']
+    ]}).then(perguntas =>{
+        res.render('index',{
+            perguntas: perguntas
+        })
+
+    })
 })
 
-app.get('/perguntar', (reg, res) =>{
+app.get('/perguntar', (req, res) => {
     res.render('perguntar')
 })
 
+app.post('/salvarpergunta', (req, res) => {
+    let titulo = req.body.titulo
+    let descricao = req.body.descricao
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() =>{
+        res.redirect('/')
+    })
+})
 
-app.listen(port, (erro) =>{
-    if(erro){
+app.listen(port, (erro) => {
+    if (erro) {
         console.log("Erro ao iniciar o servidor")
-    }else{
+    } else {
         console.log(`Servidor rodando em http://localhost:${port}`)
     }
 })
